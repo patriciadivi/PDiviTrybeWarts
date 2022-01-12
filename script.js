517bd13eb979091f2a5f7be6402f57109f92bbd3
Lindoelson Lopes <lindoelson_lopes@hotmail.com>
2022-01-12 12:34:10 -0300

    Correção de alguns erros com npm rum lint no arquivo script.js

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ const buttom = document.querySelector('.btnLogin');
 const email = document.querySelector('.btnEmail');
 const senha = document.querySelector('.btnSenha');
 
-function testebuttom(event){
-    let compEmail = "tryber@teste.com";
-    let compSenha = "123456";
-    event.preventDefault();
-    if (email.value === compEmail && senha.value === compSenha) {
-        alert("Olá, Tryber!");
-        return;
-    } else {
-        alert("Email ou senha inválidos.");
-    }
+function testebuttom(event) {
+  const compEmail = 'tryber@teste.com';
+  const compSenha = '123456';
+  event.preventDefault();
+  if (email.value === compEmail && senha.value === compSenha) {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
 }
-buttom.addEventListener("click", testebuttom);
+buttom.addEventListener('click', testebuttom);
