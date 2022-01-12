823733080619ace446f4e540976ecbae29174dd2
PatriciaDivi <patriciavianad@gmail.com>
2022-01-11 22:11:53 -0300

    3 Requisito | Acrescente um formulário de login no canto direito da barra superior contendo os inputs de email, senha e um botão de login | :rocket:

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
- 
+const buttom = document.querySelector('.btnLogin')
+const email = document.querySelector('.btnEmail')
+const senha = document.querySelector('.btnSenha')
+
+function testebuttom(event){
+    let compEmail = "tryber@teste.com";
+    let compSenha = "123456";
+    event.preventDefault();
+    if (email.value === compEmail && senha.value === compSenha) {
+        alert("Olá, Tryber!");
+        return;
+    } else {
+        alert("Email ou senha inválidos.");
+
+    }
+
+}
+
+buttom.addEventListener("click", testebuttom)
